8879876f2266f67defbbad4a32c6648f7df8b3df
mstp <EHStudent230@hotmail.com>
2024-04-15 20:44:10 -0400

    event/init

---
+++ events/__init__.py
@@ -0,0 +1,12 @@
+from event import Event
+from event_dao import EventDao
+
+#event = Event("Johnny Rupy","2024-05-05","Stade Pepis",120)
+#message = EventDao.ajouter_evenement(event)
+#print(message)
+
+#event = EventDao.lister_evenements()
+#print(event)
+
+#event = EventDao.supprimer_evenement(2)
+#print(event)
