ce98dbb3ae6f1ec71eaf3b3684f42b9569212d93
mstp <EHStudent230@hotmail.com>
2024-04-23 15:39:06 -0400

    events

--- events/event.py
+++ events/event.py
@@ -1,5 +1,5 @@
 class Event:
-    def __init__(self, nom, date, emplacement, prix):
+    def __init__(self, nom, date, emplacement, prix, images, description):
         self.__nom = nom
         self.__date = date
         self.__emplacement = emplacement
@@ -36,3 +36,19 @@ class Event:
     @prix.setter
     def prix(self, value):
         self.__prix = value
+
+    @property
+    def images(self):
+        return self.__images
+
+    @images.setter
+    def images(self, value):
+        self.__images = value
+
+    @property
+    def description(self):
+        return self.__description
+
+    @description.setter
+    def description(self, value):
+        self.__description = value
